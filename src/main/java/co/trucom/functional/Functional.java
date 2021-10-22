e2604bb1bc5c035aa96e392568de865170b0c4c6
gromao <guilherme.romao@sebasi.pt>
2021-10-22 11:43:17 +0100

    Inserting also functions UnaryOperator and BinaryOperator

--- src/main/java/co/trucom/functional/Functional.java
+++ src/main/java/co/trucom/functional/Functional.java
@@ -1,10 +1,12 @@
 package co.trucom.functional;
 
 import java.util.List;
+import java.util.function.BinaryOperator;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
 
 public class Functional {
 
@@ -30,6 +32,12 @@ public class Functional {
 		Consumer<Integer> square = (num) -> 
 			System.out.println("Square of " + num + ": " + (int) Math.pow(num, 2));
 		square.accept(16);
+
+		UnaryOperator<Integer> doubleValue = (num) -> num * 2;
+		System.out.println("Double of 987: " + doubleValue.apply(987));
+
+		BinaryOperator<Integer> sum = (x, y) -> x + y;
+		System.out.println("Sum of 15 and 762: "  + sum.apply(15,  762));
 
 	}
 
